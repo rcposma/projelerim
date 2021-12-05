bf9fea048e6683fbe9b19c4b9eddaa5efe420e7d
24patron24 <91001905+24patron24@users.noreply.github.com>
2021-12-05 17:09:44 +0300

    Add files via upload
    C dilinde yazılmış projeler

---
+++ hesapmakinesi.c
@@ -0,0 +1,55 @@
+int main()
+{
+	
+	char *locale;
+     locale = setlocale(LC_ALL, "");
+	
+    int toplama,cikarma,carpma,bolme,sayi1,sayi2;
+    float sonuc;
+    char islem;
+    printf("Toplamak icin: +\nÇýkarmak için: - \nBolmek için : / \nÇarpmak için : * \n");
+    scanf ("%c",&islem);
+    
+    if (islem=='+')
+    {
+    printf("toplama iþlemi \n");
+    printf ("sayi giriniz");
+    scanf("%d",&sayi1);
+    printf("bir sayý daha giriniz");
+    scanf("%d",&sayi2);
+    sonuc=sayi1+sayi2;
+    printf("sonuc=%f \n",sonuc );
+    }
+    else if (islem=='-')
+    {
+    printf("çýkarma iþlemi \n");
+    printf ("sayi giriniz");
+    scanf("%d",&sayi1);
+    printf("bir sayý daha giriniz");
+    scanf("%d",&sayi2);
+    sonuc=sayi1-sayi2;
+    printf("sonuc=%f \n",sonuc );
+    }
+    else if (islem=='*')
+    {
+    printf("carpma iþlemi \n");
+    printf ("sayi giriniz");
+    scanf("%d",&sayi1);
+    printf("bir sayý daha giriniz");
+    scanf("%d",&sayi2);
+    sonuc=sayi1*sayi2;
+    printf("sonuc=%f \n",sonuc );
+    }
+    else if (islem=='/')
+    {
+    printf("bölme iþlemi \n");
+    printf ("sayi giriniz");
+    scanf("%d",&sayi1);
+    printf("bir sayý daha giriniz");
+    scanf("%d",&sayi2);
+    sonuc=sayi1/sayi2;
+    printf("sonuc=%f \n",sonuc );
+    }
+
+    return 0;
+}
